94d1082c51d74a5c0177ce6e9fdc1327e640f0a8
Steven Sosebee <swsosebee@gmail.com>
2021-09-07 19:59:44 -0400

    improved meetingSave function to overwrite existing data and reload data from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,7 @@ function colorCode(time) {
     }
 }
 function createCalendar() {
+    console.log("createCalendar");
     var workingHours = moment.duration(endTime.diff(startTime)).asHours();
     for (var i = 0; i < workingHours; i++) {
         var timeBlock = $("<li data-hour='' class='row time-block'>");
@@ -52,24 +53,49 @@ function createCalendar() {
     };
 };
 
-function filterCurrentDate() {
+function updateCurrentMeeting(mtgTitle, mtgHour, mtgDate) {
+    console.log("Find current meeting: " + mtgDate + " " + mtgHour)
+    for (var i = 0; i < agenda.length; i++) {
+        // console.log(i);
+        console.log(agenda[i]);
+        if (agenda[i].date == mtgDate && agenda[i].hour == mtgHour) {
+            agenda[i] = mtgTitle;
+            // agenda.splice(i, 1);
+            break;
+          }
+     }
     
-}
+    // var dateAgenda = agenda.filter(todayAgenda => { return todayAgenda.date == mtgDate });
+    // var hourAgenda;
+    //     // console.log(dateAgenda);
+    //     // console.log($(this).data("hour"));
+    //     // console.log($(this).children("input").val())
+    //     hourAgenda = dateAgenda.filter(nowAgenda => {return nowAgenda.hour == mtgHour});
+    //     // console.log(!hourAgenda.length);
+    //     // console.log(hourAgenda[0].title);
+    //     if (hourAgenda.length) {
+    //         $(this).children("input").val(hourAgenda[0].title);
+    //     };
+};
 function populateCalendar() {
+    console.log("populateCalendar");
+    if (agenda===null) {
+        agenda = [];
+    }
     var timeBlockEl = $("li.time-block");
-    console.log(currentDayEl.text);
-    console.log(moment("Tue September 07, 2021", "ddd MMMM DD, YYYY").format("DD/MMM/YYYY"));
+    // console.log(currentDayEl.text);
+    // console.log(moment("Tue September 07, 2021", "ddd MMMM DD, YYYY").format("DD/MMM/YYYY"));
     var dateAgenda = agenda.filter(todayAgenda => { return todayAgenda.date == "07/Sep/2021" });
     
-    console.log(timeBlockEl);
+    // console.log(timeBlockEl);
     timeBlockEl.each(function(i){
     // for (var i = 0; i < hourEl.length; i++){
         var hourAgenda;
-        console.log(dateAgenda);
-        console.log($(this).data("hour"));
+        // console.log(dateAgenda);
+        // console.log($(this).data("hour"));
         // console.log($(this).children("input").val())
         hourAgenda = dateAgenda.filter(nowAgenda => {return nowAgenda.hour == $(this).data("hour")});
-        console.log(!hourAgenda.length);
+        // console.log(!hourAgenda.length);
         // console.log(hourAgenda[0].title);
         if (hourAgenda.length) {
             $(this).children("input").val(hourAgenda[0].title);
@@ -78,18 +104,53 @@ function populateCalendar() {
 };
 
 function saveMeeting(mtgTitle, mtgHour, mtgDate, mtgStatus) {
-    if (agenda === null) {
-        agenda = [];
+    console.log("saveMeeting");
+    if (agenda.length == 0) {
+        meeting.title = mtgTitle;
+        meeting.hour = mtgHour;
+        meeting.date = mtgDate;
+        meeting.status = mtgStatus;
+        console.log(meeting);
+        agenda.push(meeting);
+        console.log("--initlized agenda");
+        localStorage.setItem("Agenda", JSON.stringify(agenda));
+        agenda = JSON.parse(localStorage.getItem("Agenda"));
+        console.log("Agenda saved");
+        return;
+    } else {
+        console.log("--loop through agenda index");
+        for (var i = 0; i < agenda.length; i++) {
+            // console.log(i);
+            // console.log(agenda[i]);
+            // console.log(agenda[i].date);
+            // console.log(agenda[i].hour);
+            // console.log(mtgHour);
+            // console.log(mtgDate);
+            if (agenda[i].date == mtgDate && agenda[i].hour == mtgHour) {
+                console.log(i + "adjusted meeting title");
+                agenda[i].title = mtgTitle;
+                // console.log(agenda[i]);
+                localStorage.setItem("Agenda", JSON.stringify(agenda));
+                agenda = JSON.parse(localStorage.getItem("Agenda"));
+                console.log("Agenda saved");
+                return;
+            };
+        };
+        console.log("--add new meeting");
+        meeting.title = mtgTitle;
+        meeting.hour = mtgHour;
+        meeting.date = mtgDate;
+        meeting.status = mtgStatus;
+        console.log(meeting);
+        agenda.push(meeting);
+        localStorage.setItem("Agenda", JSON.stringify(agenda));
+        agenda = JSON.parse(localStorage.getItem("Agenda"));
+        console.log("Agenda saved");
     };
-    meeting.title = mtgTitle;
-    meeting.hour = mtgHour;
-    meeting.date = mtgDate;
-    meeting.status = mtgStatus;
-    console.log(meeting);
-    agenda.push(meeting);
-    localStorage.setItem("Agenda",JSON.stringify(agenda));
-}
+};
+
 function init() {
+    console.log("init");
     currentDayEl.html(currentTime.format("ddd MMMM DD, YYYY"));
     createCalendar();
     rootEl.append(dayEl);
